cd6a9caa8257e199d8cf4533164096d2d439db96
rush <cheje88@gmail.com>
2025-12-26 22:49:21 -0700

    dotfiles iniciales

---
+++ .local/bin/3mon.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 900x0 --rotate normal --output DP-0 --mode 1440x900 --pos 0x0 --rotate right --output DP-1 --off --output eDP-1-1 --mode 1920x1080 --pos 3460x0 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off
---
+++ .local/bin/autostart.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+picom &
+nitrogen --random --set-zoom-fill &
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+sxhkd &
+xscreensaver -no-splash &
+numlockx on &
+xfce4-power-manager &
+# nm-applet &
+clipit &
+dunst &
+# udiskie -t &
+xclip &
+# lxsession &
+dwmblocks &
+
